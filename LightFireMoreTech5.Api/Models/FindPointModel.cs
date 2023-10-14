ee4b4a63a999c8b4214e3f8602611595baee6691
Комлев Данила <komlevdanila742@gmail.com>
2023-10-14 11:34:55 +0300

    Поддержка дистанции и времени маршрута. Маршрут до банкомата. Комментарии для swagger

--- LightFireMoreTech5.Api/Models/FindPointModel.cs
+++ LightFireMoreTech5.Api/Models/FindPointModel.cs
@@ -2,8 +2,19 @@
 {
 	public class FindPointModel
 	{
+		/// <summary>
+		/// Текущая широта пользователя
+		/// </summary>
 		public double Latitude { get; set; }
+
+		/// <summary>
+		/// Текущая долгота пользователя
+		/// </summary>
 		public double Longitude { get; set; }
+
+		/// <summary>
+		/// Радиус, в котором ищем отделния и банкоматы
+		/// </summary>
 		public double Radius { get; set; }
 	}
 }
--- LightFireMoreTech5.Api/Models/Routes/Route.cs
+++ LightFireMoreTech5.Api/Models/Routes/Route.cs
@@ -2,7 +2,24 @@
 {
 	public class Route
 	{
+		/// <summary>
+		/// Тип маршрута
+		/// </summary>
 		public RouteType Type { get; set; }
+
+		/// <summary>
+		/// Длина маршрута в метрах
+		/// </summary>
+		public double DistanceMetres { get;set; }
+
+		/// <summary>
+		/// Время, которое займет маршрут в секундах
+		/// </summary>
+		public int TimeSeconds { get; set; }
+
+		/// <summary>
+		/// Точки маршрута
+		/// </summary>
 		public RoutePoint[] Points { get; set; }
 	}
 }
